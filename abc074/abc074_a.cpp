c02bc6f56f605d6152ac82757eef31dab7301501
AutoCloneBot <acautoclone@bot.com>
2024-06-26 14:47:25 +0000

    Added my codes from AtCoder

---
+++ abc074/abc074_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  int A;
+  
+  cin >> N;
+  cin >> A;
+  
+  cout << N * N - A << endl;
+}
---
+++ abc076/abc076_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int R;
+  int G;
+  
+  cin >> R;
+  cin >> G;
+  
+  cout << 2 * G - R << endl;
+}
